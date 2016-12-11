1867398e4217f6b25c3523ecbcd80456b060100b
João Vítor Dias <joaodias@noctus.org>
2016-12-11 03:37:11 -0200

    test

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -47,6 +47,7 @@ class RouteServiceProvider extends ServiceProvider
         $options = [
             'middleware' => 'auth:api',
             'namespace'  => $this->namespace,
+            'test' => 'test',
         ];
 
         Route::group($options, function ($router) {
