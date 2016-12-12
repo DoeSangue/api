a18525b42a34a0dc6786bb4608b122b8b326ec17
João Vítor Dias <joaodias@noctus.org>
2016-12-12 18:40:53 -0200

    Throttles Logins

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -3,23 +3,52 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
-use JWTAuth;
+use Illuminate\Foundation\Auth\ThrottlesLogins;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use JWTAuth;
+use Lang;
 
 class LoginController extends Controller
 {
+    use ThrottlesLogins;
+
     public function login(Request $request)
     {
+        if ($this->hasTooManyLoginAttempts($request)) {
+            $this->fireLockoutEvent($request);
+
+            return $this->sendLockoutResponse($request);
+        }
+
         $credentials = $request->only('email', 'password');
 
         try {
-            if (! $token = JWTAuth::attempt($credentials)) {
-                return response()->json(['error' => 'invalid_credentials'], 401);
+            if (!$token = JWTAuth::attempt($credentials)) {
+                $this->incrementLoginAttempts($request);
+
+                return response()->json(['error' => 'invalid_credentials'], Response::HTTP_UNAUTHORIZED);
             }
         } catch (JWTException $e) {
-            return response()->json(['error' => 'could_not_create_token'], 500);
+            return response()->json(['error' => 'could_not_create_token'], Response::HTTP_INTERNAL_SERVER_ERROR);
         }
 
         return response()->json(compact('token'));
+    }
+
+    public function username()
+    {
+        return 'email';
+    }
+
+    protected function sendLockoutResponse(Request $request)
+    {
+        $seconds = $this->limiter()->availableIn(
+            $this->throttleKey($request)
+        );
+
+        $message = Lang::get('auth.throttle', ['seconds' => $seconds]);
+
+        return response()->json(['error' => $message], Response::HTTP_TOO_MANY_REQUESTS);
     }
 }
